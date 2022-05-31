90bffe2dc32a233d6de9d20db74f277b5ff8fb48
liuboto <radorossum@gmail.com>
2022-05-31 15:48:29 -0400

    some palette work in t07

--- tests/t07.js
+++ tests/t07.js
@@ -1,5 +1,5 @@
 
-// import *  as p from "paper";
+//import *  as p from "paper";
 
 //const { project } = require("paper/dist/paper-core");
 
@@ -11,19 +11,18 @@
 var gui = new dat.GUI();
 
 var paramActions = {
-    clear: function () {project.activeLayer.removeChildren()},
-    selectAll: function () {project.activeLayer.selected = true},
-    clearSelection: function () {},
+    clear: function () { project.activeLayer.removeChildren() },
+    selectAll: function () { project.activeLayer.selected = true },
+    clearSelection: function () { },
 }
 var guiActions = gui.addFolder('Actions');
 
 guiActions.add(paramActions, 'clear');
-guiActions.add(paramActions,'selectAll');
+guiActions.add(paramActions, 'selectAll');
 
 //gui.add(clearButton= { clear:function(){ project.activeLayer.removeChildren() }},'clear');
 
 var guiGenerate = gui.addFolder('Generate');
-
 var paramRndBlobs = {
 
     paths: 5,
@@ -44,15 +43,70 @@ guiGenRndBlobs.add(paramRndBlobs, 'create');
 guiGenerate.open();
 guiGenRndBlobs.open();
 
-var paletteFolder = gui.addFolder('Palette');
+// Palette
+///////////////////////////////////
+var paramPalette = {
+    ncolors: 5,
+    color0: '#000000',
+    prob0: .5,
+    color1: '#ff0000',
+    prob1: .5,
+    color2: '#00ff00',
+    prob2: 0.,
+    color3: '#0000ff',
+    prob3: 0.,
+    color4: '#ffff00',
+    prob4: 0.,
+    color5: '#00ffff',
+    prob5: 0.,
+    palette: [],
+    colors: [],
+    colorWeights: [],
+    build: function () {
+        this.colors = [this.color0, this.color1, this.color2, this.color3, this.color4, this.color5];
+        this.colorWeights = [this.prob0, this.prob1, this.prob2, this.prob3, this.prob4, this.prob5];
+        //normalize colorweights using reduce
+        var sum = this.colorWeights.reduce(function (a, b) { return a + b; });
+        this.colorWeights = this.colorWeights.map(function (x) { return x / sum; });
+        //this.colorWeights = this.colorWeights.map(x =>{return x/this.colorWeights.reduce((a,b)=>{return a+b;},0)},this);
+        this.palette = [];
+        for (var i = 0; i < this.colors.length; i++) {
+            for (var j = 0; j < this.colorWeights[i] * this.ncolors; j++) {
+                this.palette.push(this.colors[i]);
+                if (this.palette.length >= this.ncolors) break;
+            }
+        }
 
+        console.log(this.colorWeights);
+        console.log(this.palette);
+    },
+    sorthue: function () { },
+    sortlightness: function () { },
+    randomize: function () { },
 
-function onResize() {
+};
 
-    view.viewSize = new Size(window.innerWidth, window.innerHeight);
+var guiPalette = gui.addFolder('Palette');
+guiPalette.add(paramPalette, 'ncolors', 1, 10).step(1);
+guiPalette.add(paramPalette, 'prob0', 0, 1).step(0.01);
+guiPalette.add(paramPalette, 'prob1', 0, 1).step(0.01);
+guiPalette.add(paramPalette, 'prob2', 0, 1).step(0.01);
+guiPalette.add(paramPalette, 'prob3', 0, 1).step(0.01);
+guiPalette.add(paramPalette, 'prob4', 0, 1).step(0.01);
+guiPalette.add(paramPalette, 'prob5', 0, 1).step(0.01);
+guiPalette.addColor(paramPalette, 'color0');
+guiPalette.addColor(paramPalette, 'color1');
+guiPalette.addColor(paramPalette, 'color2');
+guiPalette.addColor(paramPalette, 'color3');
+guiPalette.addColor(paramPalette, 'color4');
+guiPalette.addColor(paramPalette, 'color5');
+guiPalette.add(paramPalette, 'build');
 
-    console.log(view.viewSize);
-}
+// Generators
+///////////////////////////////////////////////
+
+//// random blobs
+///////////////////////////////////////////
 
 function createRandomBlobs() {
     var radiusDelta = paramRndBlobs.maxRadius - paramRndBlobs.minRadius;
@@ -82,9 +136,7 @@ function createBlob(center, maxRadius, points) {
     return path;
 }
 
-
-///////////////////////////////////////////////
-//// Tools
+// Tools
 ///////////////////////////////////////////////
 
 var hitOptions = {
@@ -102,7 +154,7 @@ function onMouseDown(event) {
 
     var hitResult = project.hitTest(event.point, hitOptions);
     if (!hitResult && !event.modifiers) {
-       project.activeLayer.selected = false;
+        project.activeLayer.selected = false;
         return;
     }
 
@@ -119,7 +171,6 @@ function onMouseDown(event) {
     if (hitResult) {
         //hitResult.selected = true;
 
-
         console.log(hitResult.item);
         if (event.item) {
             // event.item.selected = true;
@@ -133,10 +184,10 @@ function onMouseDown(event) {
         } else if (hitResult.type == 'stroke') {
             var location = hitResult.location;
             selectedSegment = selectedPath.insert(location.index + 1, event.point);
-            //selectedPath.smooth();
+            selectedPath.smooth();
         }
     }
-  //  movePath = hitResult.type == 'fill';
+    //  movePath = hitResult.type == 'fill';
     if (hitResult && hitResult.type == 'fill') {
         project.activeLayer.addChild(hitResult.item);
     }
@@ -167,7 +218,7 @@ function onMouseDrag(event) {
         project.selectedItems.forEach(
             function (item) {
                 //item.rotation += event.delta.angle * 0.1;
-                item.rotate(event.delta.angle * 0.1,event.point);
+                item.rotate(event.delta.angle * 0.1, event.point);
                 //item.position += event.delta;
             }
         );
@@ -177,19 +228,25 @@ function onMouseDrag(event) {
     if (Key.isDown('s')) {
         project.selectedItems.forEach(
             function (item) {
-                item.scale(1 + (event.delta.x + event.delta.y) * 0.01);
+                item.scale(1 + (event.delta.x - event.delta.y) * 0.01);
 
             }
         );
     }
-
-
 
 }
 
 function onMouseUp(event) {
     // if (!event.modifiers.shift) {
     //     project.activeLayer.selected = false;
-        
+
     // }
+}
+
+// global events
+///////////////////////////////////////////////
+function onResize() {
+
+    view.viewSize = new Size(window.innerWidth, window.innerHeight);
+    console.log("resized to " + view.viewSize.width + "," + view.viewSize.height)
 }
